c2cdb05c3772e0c195f88955b1c6bcfab31ecba5
derbestejohannesboss@gmail.com <derbestejohannesboss@gmail.com>
2025-10-13 21:53:44 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,3 +7,11 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    print("hello")
+    "self.button_1" == "ja"
+    pass
+    
+  
